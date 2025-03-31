3b1695cb5fd89ab2d892181ea759936b54ecc3e1
ismail <90913306+25Ismail@users.noreply.github.com>
2025-03-31 10:08:46 +0200

    Skapade meny-route och kopplade /menu till menu.json

--- Grupp-6-Airbean-main/app.js
+++ Grupp-6-Airbean-main/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
-const app= express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
-
-
+// Routes
+const menuRoute = require("./routes/menu");
+app.use("/menu", menuRoute);
 
 module.exports = app;
--- Grupp-6-Airbean-main/server.js
+++ Grupp-6-Airbean-main/server.js
@@ -1,7 +1,11 @@
-const { ConsoleMessage } = require("puppeteer");
+// Importerar Express-appen från app.js (där vi har satt upp middleware och routes)
 const app = require("./app");
+
+// Hämtar portnumret från miljövariabler (om det finns), annars används 3000
 const PORT = process.env.PORT || 3000;
 
+// Startar servern och lyssnar på den angivna porten
+// När servern startar, skrivs ett meddelande ut i konsolen
 app.listen(PORT, () => {
-    console.log(`kör på http:/localhost:${PORT}`)
+  console.log(`kör på http://localhost:${PORT}`);
 });
