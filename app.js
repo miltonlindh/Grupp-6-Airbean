4297b09bb49cb358b633d84a5935e6ccf0581857
miltonlindh <milton.lindh@live.com>
2025-03-31 10:17:16 +0200

    cart sprint 1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
 const app= express();
 
+const cartRoutes = require("./routes/cartRoutes");
 app.use(cors());
 app.use(express.json());
 
-
+app.use("./api/cart", cartRoutes);
 
 
 
