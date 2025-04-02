2841d7c617ecb6a348ccc885f8c4eadc94e10858
miltonlindh <milton.lindh@live.com>
2025-04-02 11:21:54 +0200

    about us

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const app= express();
 
 const cartRoutes = require("./routes/cartRoutes");
+const aboutRoutes = require("./routes/aboutRoutes")
+
 app.use(cors());
 app.use(express.json());
 
 app.use("./api/cart", cartRoutes);
-
+app.use("/api/about", aboutRoutes);
 
 
 module.exports = app;
